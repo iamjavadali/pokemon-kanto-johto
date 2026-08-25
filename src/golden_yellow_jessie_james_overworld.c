060fb0d29b36a4669801302a0b1a752c93e91929
Javad <38925895+iamjavadali@users.noreply.github.com>
2026-08-25 17:26:42 -0400

    fix: resize Jessie and James overworld sprites

--- src/golden_yellow_jessie_james_overworld.c
+++ src/golden_yellow_jessie_james_overworld.c
@@ -4,13 +4,13 @@
 #include "constants/event_objects.h"
 #include "constants/event_object_movement.h"
 
-#define GOLDEN_YELLOW_ROCKET_FRAME_SIZE 512
+#define GOLDEN_YELLOW_ROCKET_FRAME_SIZE 256
 #define GOLDEN_YELLOW_ROCKET_FRAME_WORDS (GOLDEN_YELLOW_ROCKET_FRAME_SIZE / sizeof(u32))
 
 static const u32 sObjectEventPic_Jessie[] =
-    INCGFX_U32("graphics/object_events/pics/people/jessie.png", ".4bpp", "-mwidth 4 -mheight 4");
+    INCGFX_U32("graphics/object_events/pics/people/jessie.png", ".4bpp", "-mwidth 2 -mheight 4");
 static const u32 sObjectEventPic_James[] =
-    INCGFX_U32("graphics/object_events/pics/people/james.png", ".4bpp", "-mwidth 4 -mheight 4");
+    INCGFX_U32("graphics/object_events/pics/people/james.png", ".4bpp", "-mwidth 2 -mheight 4");
 
 #define ROCKET_FRAME(gfx, frame) \
     { .data = (gfx) + GOLDEN_YELLOW_ROCKET_FRAME_WORDS * (frame), .size = GOLDEN_YELLOW_ROCKET_FRAME_SIZE }
@@ -103,15 +103,15 @@ const struct ObjectEventGraphicsInfo gObjectEventGraphicsInfo_Jessie =
     .paletteTag = OBJ_EVENT_PAL_TAG_NPC_PINK,
     .reflectionPaletteTag = OBJ_EVENT_PAL_TAG_NONE,
     .size = GOLDEN_YELLOW_ROCKET_FRAME_SIZE,
-    .width = 32,
+    .width = 16,
     .height = 32,
     .paletteSlot = PALSLOT_NPC_2,
     .shadowSize = SHADOW_SIZE_M,
     .inanimate = FALSE,
     .compressed = FALSE,
     .tracks = TRACKS_FOOT,
-    .oam = &gObjectEventBaseOam_32x32,
-    .subspriteTables = sOamTables_32x32,
+    .oam = &gObjectEventBaseOam_16x32,
+    .subspriteTables = sOamTables_16x32,
     .anims = sAnimTable_JessieJames,
     .images = sPicTable_Jessie,
 };
@@ -122,15 +122,15 @@ const struct ObjectEventGraphicsInfo gObjectEventGraphicsInfo_James =
     .paletteTag = OBJ_EVENT_PAL_TAG_NPC_PINK,
     .reflectionPaletteTag = OBJ_EVENT_PAL_TAG_NONE,
     .size = GOLDEN_YELLOW_ROCKET_FRAME_SIZE,
-    .width = 32,
+    .width = 16,
     .height = 32,
     .paletteSlot = PALSLOT_NPC_2,
     .shadowSize = SHADOW_SIZE_M,
     .inanimate = FALSE,
     .compressed = FALSE,
     .tracks = TRACKS_FOOT,
-    .oam = &gObjectEventBaseOam_32x32,
-    .subspriteTables = sOamTables_32x32,
+    .oam = &gObjectEventBaseOam_16x32,
+    .subspriteTables = sOamTables_16x32,
     .anims = sAnimTable_JessieJames,
     .images = sPicTable_James,
 };
