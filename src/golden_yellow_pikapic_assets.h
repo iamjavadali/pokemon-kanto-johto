5fef529362d993b303251f7005a5bc0fd9713d15
Javad <38925895+iamjavadali@users.noreply.github.com>
2026-08-29 17:18:16 -0400

    fix: expose Yellow portrait graphics dependencies

---
+++ src/golden_yellow_pikapic_assets.h
@@ -0,0 +1,68 @@
+#ifndef GUARD_GOLDEN_YELLOW_PIKAPIC_ASSETS_H
+#define GUARD_GOLDEN_YELLOW_PIKAPIC_ASSETS_H
+
+// Keep these INCGFX calls direct and literal so tools/scaninc can discover
+// every generated graphics dependency during a clean/full build.
+static const u8 sPikaGfx_e4000[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e4000.png", ".4bpp");
+static const u8 sPikaGfx_e40cc[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e40cc.png", ".4bpp");
+static const u8 sPikaGfx_e411c[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e411c.png", ".4bpp");
+static const u8 sPikaGfx_e41d2[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e41d2.png", ".4bpp");
+static const u8 sPikaGfx_e4272[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e4272.png", ".4bpp");
+static const u8 sPikaGfx_e4323[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e4323.png", ".4bpp");
+static const u8 sPikaGfx_e4383[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e4383.png", ".4bpp");
+static const u8 sPikaGfx_e444b[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e444b.png", ".4bpp");
+static const u8 sPikaGfx_e458b[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e458b.png", ".4bpp");
+static const u8 sPikaGfx_e463b[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e463b.png", ".4bpp");
+static const u8 sPikaGfx_e467b[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e467b.png", ".4bpp");
+static const u8 sPikaGfx_e472e[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e472e.png", ".4bpp");
+static const u8 sPikaGfx_e476e[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e476e.png", ".4bpp");
+static const u8 sPikaGfx_e4841[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e4841.png", ".4bpp");
+static const u8 sPikaGfx_e49d1[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e49d1.png", ".4bpp");
+static const u8 sPikaGfx_e4a99[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e4a99.png", ".4bpp");
+static const u8 sPikaGfx_e4b39[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e4b39.png", ".4bpp");
+static const u8 sPikaGfx_e4bde[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e4bde.png", ".4bpp");
+static const u8 sPikaGfx_e4c3e[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e4c3e.png", ".4bpp");
+static const u8 sPikaGfx_e4ce0[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e4ce0.png", ".4bpp");
+static const u8 sPikaGfx_e4e70[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e4e70.png", ".4bpp");
+static const u8 sPikaGfx_e5000[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e5000.png", ".4bpp");
+static const u8 sPikaGfx_e50af[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e50af.png", ".4bpp");
+static const u8 sPikaGfx_e523f[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e523f.png", ".4bpp");
+static const u8 sPikaGfx_e52fe[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e52fe.png", ".4bpp");
+static const u8 sPikaGfx_e548e[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e548e.png", ".4bpp");
+static const u8 sPikaGfx_e5541[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e5541.png", ".4bpp");
+static const u8 sPikaGfx_e56d1[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e56d1.png", ".4bpp");
+static const u8 sPikaGfx_e5794[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e5794.png", ".4bpp");
+static const u8 sPikaGfx_e5924[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e5924.png", ".4bpp");
+static const u8 sPikaGfx_e59ed[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e59ed.png", ".4bpp");
+static const u8 sPikaGfx_e5b7d[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e5b7d.png", ".4bpp");
+static const u8 sPikaGfx_e5c4d[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e5c4d.png", ".4bpp");
+static const u8 sPikaGfx_e5ddd[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e5ddd.png", ".4bpp");
+static const u8 sPikaGfx_e5e90[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e5e90.png", ".4bpp");
+static const u8 sPikaGfx_e6020[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e6020.png", ".4bpp");
+static const u8 sPikaGfx_e61b0[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e61b0.png", ".4bpp");
+static const u8 sPikaGfx_e6340[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e6340.png", ".4bpp");
+static const u8 sPikaGfx_e63f7[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e63f7.png", ".4bpp");
+static const u8 sPikaGfx_e6587[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e6587.png", ".4bpp");
+static const u8 sPikaGfx_e6646[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e6646.png", ".4bpp");
+static const u8 sPikaGfx_e67d6[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e67d6.png", ".4bpp");
+static const u8 sPikaGfx_e682f[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e682f.png", ".4bpp");
+static const u8 sPikaGfx_e69bf[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e69bf.png", ".4bpp");
+static const u8 sPikaGfx_e6b4f[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e6b4f.png", ".4bpp");
+static const u8 sPikaGfx_e6cdf[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e6cdf.png", ".4bpp");
+static const u8 sPikaGfx_e6e6f[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e6e6f.png", ".4bpp");
+static const u8 sPikaGfx_e6fff[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e6fff.png", ".4bpp");
+static const u8 sPikaGfx_e718f[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e718f.png", ".4bpp");
+static const u8 sPikaGfx_e731f[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e731f.png", ".4bpp");
+static const u8 sPikaGfx_e74af[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e74af.png", ".4bpp");
+static const u8 sPikaGfx_e763f[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e763f.png", ".4bpp");
+static const u8 sPikaGfx_e77cf[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e77cf.png", ".4bpp");
+static const u8 sPikaGfx_e7863[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e7863.png", ".4bpp");
+static const u8 sPikaGfx_e79f3[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e79f3.png", ".4bpp");
+static const u8 sPikaGfx_e7b83[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e7b83.png", ".4bpp");
+static const u8 sPikaGfx_e7d13[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_e7d13.png", ".4bpp");
+static const u8 sPikaGfx_f0abf[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_f0abf.png", ".4bpp");
+static const u8 sPikaGfx_f0b64[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_f0b64.png", ".4bpp");
+static const u8 sPikaGfx_f0cf4[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_f0cf4.png", ".4bpp");
+static const u8 sPikaGfx_f0d82[] = INCGFX_U8("graphics/golden_yellow/pikachu_portraits/yellow/unknown_f0d82.png", ".4bpp");
+
+#endif // GUARD_GOLDEN_YELLOW_PIKAPIC_ASSETS_H
